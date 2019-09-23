54b9f368cd5f01db887a5ffafe89d39b3d988dc5
Valera Lyuty <lvv0490@gmail.com>
2019-09-23 22:49:43 +0300

    other little changes

--- app/src/main/java/edu/example/stafflist/MainActivity.java
+++ app/src/main/java/edu/example/stafflist/MainActivity.java
@@ -1,14 +1,29 @@
 package edu.example.stafflist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import edu.example.stafflist.fragments.ListFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListFragment staffListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        staffListFragment = new ListFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.fragment_container,staffListFragment);
+        ft.commit();
+
+
     }
+
+
 }
