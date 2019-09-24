f12e328a8eb9cabd8ac294ec86c52c44125d1305
Valera Lyuty <lvv0490@gmail.com>
2019-09-24 21:06:36 +0300

    add staff model

---
+++ app/src/main/java/edu/example/stafflist/model/Staff.java
@@ -0,0 +1,52 @@
+package edu.example.stafflist.model;
+
+import java.util.UUID;
+
+public class Staff {
+
+    private UUID id;
+    private String name;
+    private int phoneNumber;
+    private int age;
+    private String gender;
+
+    public Staff() {
+        id =UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
