44d14e677ea9dde0ca97ced9f6fa6c271699a8ef
Valera Lyuty <lvv0490@gmail.com>
2019-09-29 18:07:14 +0300

    add validation

--- app/src/main/java/edu/example/stafflist/fragments/CreateNewStaffFragment.java
+++ app/src/main/java/edu/example/stafflist/fragments/CreateNewStaffFragment.java
@@ -5,19 +5,22 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import com.google.gson.Gson;
 import edu.example.stafflist.R;
 import edu.example.stafflist.model.Constans;
 import edu.example.stafflist.model.Staff;
 
 
-public class CreateNewStaffFragment extends Fragment {
+public class CreateNewStaffFragment extends Fragment implements View.OnTouchListener {
 
     private EditText etName, etAge, etNumber;
     private RadioGroup rgGender;
@@ -45,23 +48,51 @@ public class CreateNewStaffFragment extends Fragment {
         etNumber = view.findViewById(R.id.etNumber);
         rgGender = view.findViewById(R.id.rgGender);
         btnAdd = view.findViewById(R.id.btnAdd);
+
+        etName.setOnTouchListener(this);
+        etAge.setOnTouchListener(this);
+        etNumber.setOnTouchListener(this);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 int selectedRb = rgGender.getCheckedRadioButtonId();
                 rbGender = view.findViewById(selectedRb);
-                Staff staff = new Staff(etName.getText().toString(),etAge.getText().toString(),etNumber.getText().toString(),rbGender.getText().toString());
-                String gsonStr = gson.toJson(staff);
-                Intent intent = new Intent(getActivity().getBaseContext(), CreateNewStaffFragment.class);
-                intent.putExtra(Constans.OtherConst.TAG, gsonStr);
-                getTargetFragment().onActivityResult(getTargetRequestCode(), Constans.OtherConst.RESULT_CODE, intent);
-                getFragmentManager().popBackStack();
-
+                validationData();
             }
         });
 
         return view;
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (v.getId()) {
+            case (R.id.etName):
+                etName.setText("");
+                break;
+            case (R.id.etAge):
+                etAge.setText("");
+                break;
+            case (R.id.etNumber):
+                etNumber.setText("");
+                break;
+        }
+        return false;
+    }
+
+    private void validationData() {
+        if(etName.getText().toString().trim().length()==0 ||
+           etAge.getText().toString().trim().length()==0 ||
+           etNumber.getText().toString().trim().length()==0 ||
+           rgGender.getCheckedRadioButtonId()==-1) {
+            Toast.makeText(getActivity(),"Enter correct data", Toast.LENGTH_LONG).show();
+        } else {
+            Staff staff = new Staff(etName.getText().toString(),etAge.getText().toString(),etNumber.getText().toString(),rbGender.getText().toString());
+            String gsonStr = gson.toJson(staff);
+            Intent intent = new Intent(getActivity(), CreateNewStaffFragment.class);
+            intent.putExtra(Constans.OtherConst.TAG, gsonStr);
+            getTargetFragment().onActivityResult(getTargetRequestCode(), Constans.OtherConst.RESULT_CODE, intent);
+            getFragmentManager().popBackStack();
+        }
+    }
 }
